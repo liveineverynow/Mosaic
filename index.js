e67dcf3d7d418efd951a686fb6d28f5b5866401c
Will Hanlen <17443406+will-hanlen@users.noreply.github.com>
2019-09-18 15:52:06 -0600

    trying to make things smaller

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ var app = express()
 app.use(express.static('public'))
 
 app.get('/download/:file', function(req, res) {
-  let samplePath = path.join(__dirname, "downloads", req.params.file)
-  res.download(samplePath)
+  res.send("hello world")
+  //let samplePath = path.join(__dirname, "downloads", req.params.file)
+  //res.download(samplePath)
 })
 
 app.listen(3000)
